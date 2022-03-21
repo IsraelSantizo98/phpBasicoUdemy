97e78140580f557e6362a389aab2f3db6f40851f
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2022-03-20 18:16:10 -0600

    Se agregan variables

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,16 @@
 <?php include 'includes/header.php';
+$nombre = "Israel";
+$_apellido = "Israel";
 
+echo $nombre;
+var_dump($nombre);
 
-
-
+define ('constante','Esto es el valor de la constante');
+echo constante;
+//Similar a js
+const constante2 = "hola 1";
+echo constante2;
+//Dos nombres
+$nombre_cliente = "Pedro";
+$nombreCliente = "Juan";
 include 'includes/footer.php';
