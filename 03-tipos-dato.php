c0ca0d52629817ec8932ecfdeae264c1c19fe653
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2022-03-20 18:33:44 -0600

    Se agregan 5 tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,19 @@
 <?php include 'includes/header.php';
 
-
-
+//Boolean puede ser true o false 1 o 0
+$logueado = false;
+var_dump($logueado);
+//Entero
+$numero = 200;
+var_dump($numero);
+//Floats
+$float = 200.8;
+var_dump($float);
+//Strings
+$nombre = "Israel";
+var_dump($nombre);
+//array 
+$array = [1,2,2];
+var_dump($array);
 
 include 'includes/footer.php';
